959e3c784b8e87af6f7f372ed9f73281140f9df9
Ben Potter <me@bpmct.net>
2022-02-25 12:47:19 -0600

    initial packer tests

---
+++ deploy-vm/packer/files/scripts/010-code-server.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# install code-server service system-wide
+export HOME=/root
+curl -fsSL https://code-server.dev/install.sh | sh
+
+# create a code-server user
+adduser --disabled-password --gecos "" coder
+echo "coder ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/coder
+usermod -aG sudo coder
+
+# copy ssh keys from root
+cp -r /root/.ssh /home/coder/.ssh
+chown -R coder:coder /home/coder/.ssh
+
+# configure code-server to use --link with the "coder" user
+mkdir -p /home/coder/.config/code-server
+touch /home/coder/.config/code-server/config.yaml
+# echo "password: " > /home/coder/.config/code-server/config.yaml
+chown -R coder:coder /home/coder/.config
+
+# start and enable code-server and our helper service
+systemctl enable --now code-server@coder
